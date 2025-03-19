54d2584a58e31eb5f2fcc3ae70700aabc6a31be0
David Saah <davesaah@gmail.com>
2025-03-19 18:09:00 +0000

    chore: add dashboard view

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import {useUsernameStore} from '@/stores/user'
+const usernameStore = useUsernameStore()
+</script>
+
+<template>
+  <div>Welcome, {{ usernameStore.username }}</div>
+</template>
