a57ef3ef937272065a7fd2ed3db48186dbfed29c
David Saah <davesaah@gmail.com>
2025-03-19 18:03:44 +0000

    feat: introduce new constants

--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,17 @@ export const HTTP_STATUS = {
   NOT_FOUND: 404,
   SERVER_ERROR: 500,
 } as const
+
+export const AUTH_API = {
+  LOGIN: 'http://localhost:3130/login',
+  LOGOUT: 'http://localhost:3130/logout',
+  REGISTER: 'http://localhost:3130/register',
+  DELETE: 'http://localhost:3130/delete',
+  UPDATE: 'http://localhost:3130/update',
+} as const
+
+export const COLORS = {
+  WARN: '',
+  ERROR: '#d33',
+  SUCCESS: '#28a745',
+} as const
