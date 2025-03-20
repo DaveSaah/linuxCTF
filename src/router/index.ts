81ce4332bee6f2092bc3789bec18b955a061361e
David Saah <davesaah@gmail.com>
2025-03-20 22:21:09 +0000

    feat: add route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import axios, { AxiosError } from 'axios'
+import { AUTH_API, HTTP_STATUS } from '@/constants'
+
+async function isAuthenticated() {
+  try {
+    const response = await axios.get(AUTH_API.IS_AUTHENTICATED, { withCredentials: true })
+    if (response.status == HTTP_STATUS.OK) {
+      return true
+    }
+  } catch (error) {
+    const axiosError = error as AxiosError
+    if (axiosError.response?.status == HTTP_STATUS.UNAUTHORIZED) {
+      return false
+    }
+  }
+}
+
+const authenticated = await isAuthenticated()
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,15 +30,36 @@ const router = createRouter({
     {
       path: '/register',
       name: 'register',
+      beforeEnter: (to, from, next) => {
+        if (authenticated) {
+          next('/dashboard')
+        } else {
+          next()
+        }
+      },
       component: () => import('../views/RegisterView.vue'),
     },
     {
       path: '/login',
       name: 'login',
+      beforeEnter: (to, from, next) => {
+        if (authenticated) {
+          next('/dashboard')
+        } else {
+          next()
+        }
+      },
       component: () => import('../views/LoginView.vue'),
     },
     {
       path: '/dashboard',
+      beforeEnter: (to, from, next) => {
+        if (authenticated) {
+          next()
+        } else {
+          next('/login')
+        }
+      },
       name: 'dashboard',
       component: () => import('../views/DashboardView.vue'),
     },
