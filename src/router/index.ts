b4c0f2abf7a9968f31394f5dd5cd67585bd76000
David Saah <davesaah@gmail.com>
2025-04-01 15:45:00 +0000

    chore: use types in place of any

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHistory,
+  type NavigationGuardNext,
+  type RouteLocationNormalized,
+  type RouteLocationNormalizedLoaded,
+} from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 import axios, { AxiosError } from 'axios'
 import { AUTH_API, HTTP_STATUS } from '@/constants'
 
 async function isAuthenticated(): Promise<boolean> {
   try {
-    const response = await axios.get(AUTH_API.IS_AUTHENTICATED, { withCredentials: true });
+    const response = await axios.get(AUTH_API.IS_AUTHENTICATED, { withCredentials: true })
     if (response.status === HTTP_STATUS.OK) {
-      return true;
+      return true
     }
   } catch (error) {
-    const axiosError = error as AxiosError;
+    const axiosError = error as AxiosError
     if (axiosError.response?.status === HTTP_STATUS.UNAUTHORIZED) {
-      return false;
+      return false
     }
   }
-  return false; // Ensure a boolean is always returned
+  return false // Ensure a boolean is always returned
 }
 
-async function requireAuth(to: any, from: any, next: any) {
+async function requireAuth(
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalizedLoaded,
+  next: NavigationGuardNext,
+) {
   if (await isAuthenticated()) {
     next()
   } else {
@@ -26,7 +36,11 @@ async function requireAuth(to: any, from: any, next: any) {
   }
 }
 
-async function redirectIfAuthenticated(to: any, from: any, next: any) {
+async function redirectIfAuthenticated(
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalizedLoaded,
+  next: NavigationGuardNext,
+) {
   if (await isAuthenticated()) {
     next('/dashboard')
   } else {
@@ -46,19 +60,19 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       beforeEnter: redirectIfAuthenticated,
-      component: () => import('../views/RegisterView.vue'),
+      component: () => import('@/views/RegisterView.vue'),
     },
     {
       path: '/login',
       name: 'login',
       beforeEnter: redirectIfAuthenticated,
-      component: () => import('../views/LoginView.vue'),
+      component: () => import('@/views/LoginView.vue'),
     },
     {
       path: '/dashboard',
       name: 'dashboard',
       beforeEnter: requireAuth,
-      component: () => import('../views/DashboardView.vue'),
+      component: () => import('@/views/DashboardView.vue'),
     },
   ],
 })
